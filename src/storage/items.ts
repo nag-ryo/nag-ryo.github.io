0734213cfab6552f48d60c9ce0948583fdc8d4eb
nag-ryo <ryo.nagamatsu@milab-inc.com>
2024-12-31 16:57:15 +0900

    項目の追加

--- src/storage/items.ts
+++ src/storage/items.ts
@@ -1,6 +1,12 @@
 /** 表示する項目をまとめた配列 */
 export const Items = [
     {
+        title: 'JS/TS',
+        desc: 'JavascriptとTypescriptについてまとめています。必須級の言語。',
+        imgSrc: '',
+        link: '/jsts',
+    },
+    {
         title: 'Angular',
         desc: 'Google製のフレームワーク Angular の使い方を学ぼう。',
         imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg',
@@ -17,6 +23,12 @@ export const Items = [
         desc: 'GitやGitHubの使い方など。',
         imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg',
         link: '/git',
+    },
+    {
+        title: 'PHP',
+        desc:'',
+        imgSrc: '',
+        link: '/php',
     },
     {
         title: 'Develop',
