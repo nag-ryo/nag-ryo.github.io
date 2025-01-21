c53d5002105156f2f389bbebee057e7c27013478
nag-ryo <ryo.nagamatsu@milab-inc.com>
2025-01-21 13:06:40 +0900

    修正

---
+++ src/pages/git/[slug].tsx
@@ -0,0 +1,34 @@
+import { getPostData, getSortedPostsData } from '@/lib/posts';
+
+export async function getStaticPaths() {
+    const allPostsData = getSortedPostsData();
+    const paths = allPostsData.map(({ slug }) => ({
+        params: { slug },
+    }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+    const postData = await getPostData(params.slug, 'git');
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export default function Post({ postData }: { postData: { title: string; contentHtml: string } }) {
+    return (
+        <>
+            <h1 className="text-4xl font-bold">{postData.title}</h1>
+            <div
+                className="markdown"
+                dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+            />
+        </>
+    );
+}
