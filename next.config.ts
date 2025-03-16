de67547a83ad3da65c3b9cdc485ed9c8299f0fa7
nag-ryo <ryo.nagamatsu@milab-inc.com>
2025-03-16 16:50:03 +0900

    webhookトリガーのために更新

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
     output: 'export',
     env: {
         NEXT_PUBLIC_BASE_PATH: '/nag-ryo.github.io',
+        MICRO_CMS_API_KEY: process.env.MICRO_CMS_API_KEY,
+        MICRO_CMS_SERVICE_DOMAIN: process.env.MICRO_CMS_SERVICE_DOMAIN,
+        MICRO_CMS_ENDPOINT_BLOG: process.env.MICRO_CMS_ENDPOINT_BLOG,
+        PREVIEW_SECRET: process.env.PREVIEW_SECRET,
     },
     images: { unoptimized: true },
 };
