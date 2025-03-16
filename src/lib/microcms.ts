8fc0356efcf2f73a8e7582c2c6c21d8453a7cc57
nag-ryo <ryo.nagamatsu@milab-inc.com>
2025-03-16 17:00:42 +0900

    更新

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -4,6 +4,6 @@ import { createClient } from 'microcms-js-sdk';
  * microCMSと接続するためのClientデータ
  */
 export const client = createClient({
-    serviceDomain: process.env.MICRO_CMS_SERVICE_DOMAIN as string,
-    apiKey: process.env.MICRO_CMS_API_KEY as string,
+    serviceDomain: process.env.MICRO_CMS_SERVICE_DOMAIN || '',
+    apiKey: process.env.MICRO_CMS_API_KEY || '',
 });
